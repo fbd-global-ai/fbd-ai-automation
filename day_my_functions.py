2947480ccd4ae301037ab800ac56b41a2985f194
fbd-global-ai <dalbudakfethibugra@gmail.com>
2025-11-12 18:55:48 +0300

    Create day_my_functions.py

---
+++ day_my_functions.py
@@ -0,0 +1,7 @@
+def introduce(name, age):
+  print(My name is" , name, "and I am", age, "years old.")
+  def multiply(x, y):
+    return x * y
+    introduce("Bugra" ,38)
+    result = multiply(6, 7)
+    print("Result is:", result)
