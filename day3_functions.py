a9c70bb04541e80a9d8a0a89e977e94df92a37e1
fbd-global-ai <dalbudakfethibugra@gmail.com>
2025-11-12 18:39:34 +0300

    Create day3_functions.py

---
+++ day3_functions.py
@@ -0,0 +1,14 @@
+def say_hello():
+  print("Hello from FBD Global AI")
+  say_hello()
+
+
+def greet(name):
+  print("Hello", name)
+
+greet("Bugra")
+greet("FBD")
+
+def add(a, b):
+  return(add(5, 3))
+  
